39c0a1b5d3631ad6bccfc266c4b9a2bf3f3fdb38
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-12-10 22:51:04 -0500

    Cordgrass Retexture

--- src/main/java/corundum/wetland_whimsy/content/blocks/CordgrassBlock.java
+++ src/main/java/corundum/wetland_whimsy/content/blocks/CordgrassBlock.java
@@ -1,29 +1,32 @@
 package corundum.wetland_whimsy.content.blocks;
 
+import com.mojang.serialization.MapCodec;
+
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.TallGrassBlock;
+import net.minecraft.world.level.block.BushBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class CordgrassBlock extends TallGrassBlock {
-	protected static final VoxelShape SHAPE = Block.box(2.0, 0.0, 2.0, 14.0, 28.0, 14.0);
+public class CordgrassBlock extends BushBlock {
+	protected static final MapCodec<CordgrassBlock> CODEC = simpleCodec(CordgrassBlock::new);
+	protected static final VoxelShape SHAPE = Block.box(2.0, 0.0, 2.0, 14.0, 24.0, 14.0);
 
-	public CordgrassBlock(Properties properties) {
+	public CordgrassBlock(BlockBehaviour.Properties properties) {
 		super(properties);
-	}
-
-	@Override
-	public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
-		// TODO: Spawn cordgrass item
+		this.registerDefaultState(this.defaultBlockState());
 	}
 
 	@Override
 	protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
 		return SHAPE;
+	}
+
+	@Override
+	protected MapCodec<? extends BushBlock> codec() {
+		return CODEC;
 	}
 }
