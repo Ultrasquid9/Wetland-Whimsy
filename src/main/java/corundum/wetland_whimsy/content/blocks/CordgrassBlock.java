5ca069a1a077da2503ab22507423696a51b3e23d
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-12-21 13:38:59 -0500

    Bonemealable Cordgrass

--- src/main/java/corundum/wetland_whimsy/content/blocks/CordgrassBlock.java
+++ src/main/java/corundum/wetland_whimsy/content/blocks/CordgrassBlock.java
@@ -3,8 +3,14 @@ package corundum.wetland_whimsy.content.blocks;
 import com.mojang.serialization.MapCodec;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,9 +18,9 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("null")
-public class CordgrassBlock extends BushBlock {
+public class CordgrassBlock extends BushBlock implements BonemealableBlock {
 	protected static final MapCodec<CordgrassBlock> CODEC = simpleCodec(CordgrassBlock::new);
-	protected static final VoxelShape SHAPE = Block.box(2.0, 0.0, 2.0, 14.0, 24.0, 14.0);
+	protected static final VoxelShape SHAPE = Block.box(1.0, 0.0, 1.0, 15.0, 24.0, 15.0);
 
 	public CordgrassBlock(BlockBehaviour.Properties properties) {
 		super(properties);
@@ -29,5 +35,20 @@ public class CordgrassBlock extends BushBlock {
 	@Override
 	protected MapCodec<? extends BushBlock> codec() {
 		return CODEC;
+	}
+
+	@Override
+	public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state) {
+		return true;
+	}
+
+	@Override
+	public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state) {
+		return true;
+	}
+
+	@Override
+	public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state) {
+		popResource(level, pos, new ItemStack(this));
 	}
 }
