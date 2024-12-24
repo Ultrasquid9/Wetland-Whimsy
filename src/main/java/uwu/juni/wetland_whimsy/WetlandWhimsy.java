534a3aedc17450d56f55b736d5101df95e4b47de
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-12-24 17:47:47 -0500

    Switch to Elysium; Nuke the Swamps
    Blueprint didnt do what I needed it to

--- src/main/java/uwu/juni/wetland_whimsy/WetlandWhimsy.java
+++ src/main/java/uwu/juni/wetland_whimsy/WetlandWhimsy.java
@@ -1,13 +1,21 @@
 package uwu.juni.wetland_whimsy;
 
 import com.mojang.logging.LogUtils;
+
+import net.jadenxgamer.elysium_api.api.biome.ElysiumBiomeRegistry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import uwu.juni.wetland_whimsy.content.Creative;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyBlocks;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyItems;
 import uwu.juni.wetland_whimsy.data.Datagen;
+import uwu.juni.wetland_whimsy.data.worldgen.WetlandWhimsyBiomesDatagen;
 import uwu.juni.wetland_whimsy.tags.WetlandWhimsyWoodTypes;
 import uwu.juni.wetland_whimsy.worldgen.WetlandWhimsyFoliagePlacers;
 import uwu.juni.wetland_whimsy.worldgen.WetlandWhimsyTrunkPlacers;
@@ -34,16 +42,29 @@ public class WetlandWhimsy {
 		WetlandWhimsyBlocks.createSignItems(); // Signs are wacky 
 		WetlandWhimsyWoodTypes.registerWoodTypes();
 
-		this.eventSetup(modEventBus);
-
-		nukeTheSwamps();
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-	public void eventSetup(IEventBus bussin) {
-		//bussin.addListener(WetlandWhimsyBlockEntities::handleBlockEntities);
-	}
+	@SubscribeEvent
+	public void nukeTheSwamps(ServerAboutToStartEvent event) {
+		var registryAccess = event.getServer().registryAccess();
 
-	private void nukeTheSwamps() {
-		// TODO: Blueprint
+		ElysiumBiomeRegistry.replaceOverworldBiome(
+			Biomes.SWAMP, 
+			WetlandWhimsyBiomesDatagen.BOG, 
+			1, 
+			24, 
+			new ResourceLocation(MODID, "nuketheswamps"),
+			registryAccess
+		);
+
+		ElysiumBiomeRegistry.replaceOverworldBiome(
+			WetlandWhimsyBiomesDatagen.BOG, 
+			WetlandWhimsyBiomesDatagen.MARSH, 
+			0.3, 
+			16, 
+			new ResourceLocation(MODID, "marshtomp"),
+			registryAccess
+		);
 	}
 }
