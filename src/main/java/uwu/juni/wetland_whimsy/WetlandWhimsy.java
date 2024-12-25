dbf493ddf6c4b9d3d2eaec027a9d767846e5d1c3
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2024-12-25 16:17:06 -0500

    a

--- src/main/java/uwu/juni/wetland_whimsy/WetlandWhimsy.java
+++ src/main/java/uwu/juni/wetland_whimsy/WetlandWhimsy.java
@@ -2,20 +2,13 @@ package uwu.juni.wetland_whimsy;
 
 import com.mojang.logging.LogUtils;
 
-import net.jadenxgamer.elysium_api.api.biome.ElysiumBiomeRegistry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.biome.Biomes;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import uwu.juni.wetland_whimsy.content.Creative;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyBlocks;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyItems;
 import uwu.juni.wetland_whimsy.data.Datagen;
-import uwu.juni.wetland_whimsy.data.worldgen.WetlandWhimsyBiomesDatagen;
 import uwu.juni.wetland_whimsy.tags.WetlandWhimsyWoodTypes;
 import uwu.juni.wetland_whimsy.worldgen.WetlandWhimsyFoliagePlacers;
 import uwu.juni.wetland_whimsy.worldgen.WetlandWhimsyTrunkPlacers;
@@ -41,21 +34,5 @@ public class WetlandWhimsy {
 
 		WetlandWhimsyBlocks.createSignItems(); // Signs are wacky 
 		WetlandWhimsyWoodTypes.registerWoodTypes();
-
-		MinecraftForge.EVENT_BUS.register(this);
-	}
-
-	@SubscribeEvent
-	public void nukeTheSwamps(ServerAboutToStartEvent event) {
-		var registryAccess = event.getServer().registryAccess();
-
-		ElysiumBiomeRegistry.replaceOverworldBiome(
-			Biomes.SWAMP, 
-			WetlandWhimsyBiomesDatagen.MARSH, 
-			0.3, 
-			16, 
-			new ResourceLocation(MODID, "marshtomp"),
-			registryAccess
-		);
 	}
 }
