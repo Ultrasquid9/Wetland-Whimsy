d27129bd791f10c055ae763bca26f33cd2e58de0
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-07 17:21:30 -0500

    Extinguish Braziers With Shovels

--- src/main/java/uwu/juni/wetland_whimsy/content/blocks/BrazierBlock.java
+++ src/main/java/uwu/juni/wetland_whimsy/content/blocks/BrazierBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
@@ -99,6 +100,13 @@ public class BrazierBlock extends Block implements SimpleWaterloggedBlock {
 		InteractionHand hand,
 		BlockHitResult hitResult
 	) {
+		if (stack.is(ItemTags.SHOVELS) && state.getValue(LIT)) {
+			level.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1.0F, 1.0F);
+			level.setBlock(pos, state.setValue(LIT, Boolean.valueOf(false)), 3);
+
+			return ItemInteractionResult.SUCCESS;
+		}
+
 		if (state.getValue(WATERLOGGED) || state.getValue(LIT))
 			return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
 
