e21efd37521706139ca93e3bae495cfbc6355393
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-01-25 17:49:44 -0500

    Scalable Reward Initial Implementaion

--- src/main/java/uwu/juni/wetland_whimsy/content/blocks/entities/AncientPotBlockEntity.java
+++ src/main/java/uwu/juni/wetland_whimsy/content/blocks/entities/AncientPotBlockEntity.java
@@ -1,28 +1,22 @@
 package uwu.juni.wetland_whimsy.content.blocks.entities;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 import javax.annotation.Nonnull;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup.Provider;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyBlockEntities;
-import uwu.juni.wetland_whimsy.misc.Config;
+import uwu.juni.wetland_whimsy.datapacks.ScalableReward;
 
 public class AncientPotBlockEntity extends BlockEntity {
 	private int lootQuality;
-	private ThreadLocalRandom random;
 
 	public AncientPotBlockEntity(BlockPos pos, BlockState state) {
 		super(WetlandWhimsyBlockEntities.ANCIENT_POT.get(), pos, state);
-		random = ThreadLocalRandom.current();
 	}
 
 	@Override
@@ -44,54 +38,13 @@ public class AncientPotBlockEntity extends BlockEntity {
 	public void dropLoot(Level level, BlockPos pos) {
 		lootQuality += 2; // Ensuring that it is not zero
 
-		var chance2 = Config.ancientPotItems.size() / (int)(lootQuality * 2.5);
-		if (chance2 <= 0) 
-			chance2 = 1;
+		var loot = ScalableReward.Manager.getLoot(
+			level.getRandom(), 
+			"ancient_coin", 
+			lootQuality
+		);
 
-		var i = 0;
-		var j = 0;
-		for (var item : Config.ancientPotItems) {
-			var thign = lootQuality == 2 ? lootQuality : (int)(lootQuality / 1.25);
-			if (random.nextInt(0, thign - random.nextInt(0, thign)) == 0) 
-				continue;
-			if (random.nextInt(0, chance2) != 0) 
-				continue;
-
-			i++;
-			if (i >= Integer.min(lootQuality, Config.ancientPotMaxDropCount)) break;
-
-			var stack = new ItemStack(BuiltInRegistries.ITEM.get(item));
-			growStack(stack);
-
-			if (stack.getCount() == 0) continue;
-
-			Block.popResource(
-				level, 
-				pos, 
-				stack
-			);
-			j++;
-		}
-
-		if (j == 0)
-			Block.popResource(
-				level, 
-				pos, 
-				new ItemStack(
-					BuiltInRegistries.ITEM.get(
-						Config.ancientPotItems.get(
-							random.nextInt(Config.ancientPotItems.size() - 3, Config.ancientPotItems.size())
-						)
-					)
-				)
-			);
-	}
-
-	private void growStack(ItemStack stack) {
-		if (stack.toString().contains("template")) return;
-		stack.grow(Integer.min(stack.getMaxStackSize(), random.nextInt(0, lootQuality)) - 1);
-
-		if (stack.isDamageableItem())
-			stack.setDamageValue(random.nextInt(1, stack.getMaxDamage() - 1));
+		for (var item : loot) 
+			Block.popResource(level, pos, item);
 	}
 }
--- src/main/java/uwu/juni/wetland_whimsy/datapacks/ScalableReward.java
+++ src/main/java/uwu/juni/wetland_whimsy/datapacks/ScalableReward.java
@@ -1,5 +1,7 @@
 package uwu.juni.wetland_whimsy.datapacks;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -8,7 +10,13 @@ import java.util.Map.Entry;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.world.item.ItemStack;
+import uwu.juni.wetland_whimsy.WetlandWhimsy;
 
 public record ScalableReward(String input, Map<String, Integer> rewards) {
 	public static final Codec<ScalableReward> CODEC = RecordCodecBuilder.create(
@@ -20,11 +28,91 @@ public record ScalableReward(String input, Map<String, Integer> rewards) {
 	);
 
 	public class Manager {
-		private static List<ScalableReward> SCALABLE_REWARDS = List.of();
+		private static List<ScalableReward> SCALABLE_REWARDS = new ArrayList<>();
 
 		public static void add(Set<Entry<ResourceKey<ScalableReward>, ScalableReward>> rewards) {
 			for (var entry : rewards)
 				SCALABLE_REWARDS.add(entry.getValue());
+
+			for (var entry : SCALABLE_REWARDS)
+				WetlandWhimsy.LOGGER.info(entry.input());
+		}
+
+		public static List<ItemStack> getLoot(
+			RandomSource random, 
+			String key, 
+			int quality
+		) {
+			HashMap<String, Integer> table = null;
+
+			for (var i : SCALABLE_REWARDS) {
+				if (i.input().contains(key)) {
+					table = new HashMap<String, Integer>(i.rewards());
+					break;
+				}
+			}
+
+			if (table == null)
+				return List.of();
+
+			List<Integer> weights = new ArrayList<>();
+			for (var i : table.values())
+				weights.add(i);
+			var average = average(weights);
+
+			var quality2 = Math.max(1, (int)((double)quality * 0.5));
+			for (var weight : table.values())
+				if (weight > average)
+					weight /= quality2;
+				else if (weight < average)
+					weight *= quality2;
+
+			List<ItemStack> list = new ArrayList<>();
+			for (var i = Math.min(random.nextInt(1, quality), 10); i > 0; i--) {
+				if (table.isEmpty())
+					break;
+
+				var builder = SimpleWeightedRandomList.<String>builder();
+				for (var j : table.entrySet())
+					builder.add(j.getKey(), j.getValue());
+				var loottable = builder.build();
+
+				var str = loottable
+					.getRandom(random)
+					.get()
+					.data();
+
+				table.remove(str);
+
+				var item = new ItemStack(
+					BuiltInRegistries.ITEM.get(
+						ResourceLocation.parse(str)
+					)
+				);
+				growStack(random, item, quality);
+				list.add(item);
+			}
+
+			return list;
+		}
+
+		private static void growStack(RandomSource random, ItemStack stack, int quality) {
+			if (stack.toString().contains("template")) return;
+			stack.grow(Integer.min(stack.getMaxStackSize(), random.nextInt(1, quality)) - 1);
+	
+			if (stack.isDamageableItem())
+				stack.setDamageValue(random.nextInt(1, stack.getMaxDamage() - 1));
+		}
+
+		private static int average(List<Integer> input) {
+			int average = 0;
+
+			for (var i : input)
+				average += i;
+
+			average /= input.size();
+
+			return average;
 		}
 	}
 }
