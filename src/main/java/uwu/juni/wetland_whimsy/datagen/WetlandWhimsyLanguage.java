e63a2139f9750f50074be68d105bc101490ef3fe
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-02-16 21:49:22 -0500

    Marsh language datagen

--- src/main/java/uwu/juni/wetland_whimsy/datagen/WetlandWhimsyLanguage.java
+++ src/main/java/uwu/juni/wetland_whimsy/datagen/WetlandWhimsyLanguage.java
@@ -97,6 +97,7 @@ public class WetlandWhimsyLanguage extends LanguageProvider {
 		addEntityType(WetlandWhimsyEntityTypes.SWAMP_SPIDER, "Swamp Spider");
 		addItem(WetlandWhimsyItems.SWAMP_SPIDER_SPAWN_EGG, "Swamp Spider Spawn Egg");
 
+		add("biome.wetland_whimsy.marsh", "Marsh");
 		add("trim_pattern.wetland_whimsy.dots", "Dots Armor Trim");
 		add("jukebox_song.wetland_whimsy.nuke_the_swamps", "Quizzly - Nuke The Swamps");
 
