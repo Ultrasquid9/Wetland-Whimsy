72e17d85c9f4d8ba55fcb4cf5d989cec762347e5
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-02-19 21:38:53 -0500

    Crane Befriending

--- src/main/java/uwu/juni/wetland_whimsy/content/entities/CraneEntity.java
+++ src/main/java/uwu/juni/wetland_whimsy/content/entities/CraneEntity.java
@@ -1,10 +1,18 @@
 package uwu.juni.wetland_whimsy.content.entities;
 
+import java.util.function.BiFunction;
+
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import com.google.common.collect.ImmutableList;
 
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.AnimationState;
@@ -14,21 +22,29 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.BreedGoal;
 import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.goal.FollowParentGoal;
+import net.minecraft.world.entity.ai.goal.LeapAtTargetGoal;
 import net.minecraft.world.entity.ai.goal.LookAtPlayerGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 import net.minecraft.world.entity.ai.goal.PanicGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.TemptGoal;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.gameevent.GameEvent;
 import uwu.juni.wetland_whimsy.content.WetlandWhimsyEntityTypes;
 import uwu.juni.wetland_whimsy.content.entities.goals.CraneFlyGoal;
 import uwu.juni.wetland_whimsy.tags.WetlandWhimsyTags;
 
 @ParametersAreNonnullByDefault
 public class CraneEntity extends Animal {
+	public boolean befriended = false;
+	private static final String befriendedId = "befriended";
+
 	public final AnimationState idleAnimationState = new AnimationState();
 	private int idleAnimationTimeout = 0;
 
@@ -43,6 +59,8 @@ public class CraneEntity extends Animal {
     protected void registerGoals() {
 		var goals = ImmutableList.of(
 			new FloatGoal(this),
+			new LeapAtTargetGoal(this, 0.4F),
+			new MeleeAttackGoal(this, 1.0, true),
 			new PanicGoal(this, 1.2),
 			new BreedGoal(this, 1.0),
 			new TemptGoal(this, 1.0, item -> isFood(item), false),
@@ -54,19 +72,42 @@ public class CraneEntity extends Animal {
 		);
 	
 		var priority = 0;
-
 		for (var goal : goals) {
 			goalSelector.addGoal(priority, goal);
 			priority++;
 		}
+
+		targetSelector.addGoal(
+			priority, 
+			new NearestAttackableTargetGoal<>(
+				this, 
+				Monster.class, 
+				blocksBuilding, 
+				e -> befriended
+			)
+		);
     }
 
 	public static AttributeSupplier.Builder createAttributes() {
 		return Animal.createLivingAttributes()
 			.add(Attributes.FALL_DAMAGE_MULTIPLIER, 0.25)
 			.add(Attributes.MOVEMENT_SPEED, 0.285)
-			.add(Attributes.MAX_HEALTH, 30.0)
+			.add(Attributes.MAX_HEALTH, 30)
+			.add(Attributes.ATTACK_DAMAGE, 4)
 			.add(Attributes.FOLLOW_RANGE, 6);
+	}
+
+	@Override
+	public void addAdditionalSaveData(CompoundTag compound) {
+		super.addAdditionalSaveData(compound);
+		compound.putBoolean(befriendedId, befriended);
+	}
+
+	@Override
+	public void readAdditionalSaveData(CompoundTag compound) {
+		super.readAdditionalSaveData(compound);
+		if (compound.contains(befriendedId))
+			befriended = compound.getBoolean(befriendedId);
 	}
 
 	private void setupAnimationStates() {
@@ -114,5 +155,70 @@ public class CraneEntity extends Animal {
 	@Override
 	public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob otherParent) {
 		return WetlandWhimsyEntityTypes.CRANE.get().create(level);
+	}
+
+	@Override
+	public InteractionResult mobInteract(Player player, InteractionHand hand) {
+		BiFunction<Player, InteractionHand, InteractionResult> fn = super::mobInteract;
+
+		if (!isFood(player.getItemInHand(hand)))
+			fn = super::mobInteract;
+
+		else if (!befriended && !isBaby())
+			fn = this::befriend;
+
+		else if (getHealth() < getMaxHealth())
+			fn = this::heal;
+
+		return fn.apply(player, hand);
+	}
+
+	private InteractionResult heal(Player player, InteractionHand hand) {
+		var item = player.getItemInHand(hand);
+		var props = item.getFoodProperties(this); // Parameter does not seem to be used
+		
+		heal(props == null ? 4 : props.nutrition() * 2);
+		usePlayerItem(player, hand, item);
+		gameEvent(GameEvent.EAT);
+
+		return InteractionResult.SUCCESS;
+	}
+
+	private InteractionResult befriend(Player player, InteractionHand hand) {
+		befriended = true;
+		usePlayerItem(player, hand, player.getItemInHand(hand));
+		sendParticles(ParticleTypes.HAPPY_VILLAGER, 10);
+
+		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	public boolean hurt(DamageSource source, float amount) {
+		if (source.getEntity() instanceof Player) {
+			if (befriended)
+				sendParticles(ParticleTypes.ANGRY_VILLAGER, 4);
+
+			befriended = false;
+		}
+
+		return super.hurt(source, amount);
+	}
+
+	private <T extends ParticleOptions> void sendParticles(T type, int amount) {
+		if (level() instanceof ServerLevel sLevel) {
+			var pos = position();
+
+			sLevel.sendParticles(
+				type, 
+				pos.x, 
+				pos.y, 
+				pos.z, 
+				amount, 
+				0.5, 
+				1, 
+				0, 
+				0
+			);
+		}
 	}
 }
